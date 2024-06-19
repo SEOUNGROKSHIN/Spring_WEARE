aaf914faab9703aa7b25b8fefc2781f8a758f85d
SHINSEOUNGROK <tnsfhr03@gmail.com>
2024-06-19 21:05:47 +0900

    first commit

---
+++ src/main/java/com/greedy/spring_weare/SpringWeareApplication.java
@@ -0,0 +1,13 @@
+package com.greedy.spring_weare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWeareApplication.class, args);
+    }
+
+}
